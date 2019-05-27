b7c56d09a16ab929356f011115df10bd9060b557
Jette Bakemeier <jette.bakemeier@gmail.com>
2019-05-27 19:03:24 +0100

    added some controller actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,31 +1,49 @@
 class BookingsController < ApplicationController
-  # def index
-  #   @bookings = Booking.all
-  # end
+  before_action :set_booking, only: [:show, :destroy, :edit]
 
-  # def show
-  #   @booking = Booking.find(params[:id])
+  def index
+    @bookings = Booking.all
+  end
 
-  # end
+  def show
+  end
 
-  # def new
-  #   @booking = Booking.new
-  # end
+  def new
+    @booking = Booking.new
+  end
 
-  # def create
-  #   @booking = Booking.new(booking_params)
-  # end
+  def create
+    @booking = Booking.create(booking_params)
+    # booking should be nested into the events and then...
+    # @booking.event = Event.find(params[:id])
+    # @booking.user = current_user
+    if @booking.save
+      redirect_to events_index_path
+    else
+      render :new
+    end
+  end
 
-  # def update
-  # end
+  # do we need update and edit here?
+  def update
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def destroy
-  # end
+  def destroy
+    @booking.delete
+    # authorize @booking
+    # redirect_to current_user
+  end
 
-  # def booking_params
-  #   params.require(:booking).permit
-  # end
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,15 @@
 class UsersController < ApplicationController
-  def index
-  end
 
   def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def update
+    # depending on what should be shown? own profile?
+    # @user = User.find(params[:id])
+    # @user = current_user
   end
 
   def edit
+  end
+
+  def update
   end
 
   def destroy
