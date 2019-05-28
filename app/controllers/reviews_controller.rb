071ae3a33156c1224fe1b8da5f9511538befd225
Isabella Ebihara <isabella.ebihara@gmail.com>
2019-05-28 11:34:33 +0100

    review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,26 @@
 class ReviewsController < ApplicationController
+before_action
+
   def index
+    @reviews = Review.all
   end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.create(review_params)
+    if @review.save
+      redirect_to events_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
