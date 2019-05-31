e89522f9e1018530802852653e6a6677dfc849a7
Aman Bharti <bsabharti@gmail.com>
2019-05-31 11:59:56 +0100

    Fixed behavior of Apply button from Event show to create status in User Show. Also made some changes to the Event Show.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_095641) do
+ActiveRecord::Schema.define(version: 2019_05_31_105630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "status", default: "pending"
+    t.string "status", default: "Pending"
     t.bigint "event_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
