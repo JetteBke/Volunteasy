7340d19d0fe4f29eed4ce9888c3a58f31c7982b9
Jette Bakemeier <jette.bakemeier@gmail.com>
2019-06-03 09:53:27 +0100

    idk

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :city, :phone, :interests, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dob, :city, :phone, :photo, :interests, :bio])
   end
 
   #@navbar_organization = Organization.where(user_id: current_user)
