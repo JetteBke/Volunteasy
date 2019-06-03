c187eb08bcd8c489a977efe4467ae3d1676dd134
Jette Bakemeier <jette.bakemeier@gmail.com>
2019-06-03 14:41:29 +0100

    review stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,19 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :organizations do
     resources :events, except: [ :index, :show]
-    resources :reviews, only: [:index]
+    resources :reviews, only: [:index ]
   end
   resources :events, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, except: [:show, :new, :create] do
+    resources :reviews, only: [:new, :create]
     member do
       post 'approve', to: 'bookings#approve'
       post 'reject', to:'bookings#reject'
     end
   end
-  resources :reviews, only: [:new, :create]
+  # resources :reviews, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
